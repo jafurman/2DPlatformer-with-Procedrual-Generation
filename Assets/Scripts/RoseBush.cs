9ac20442c6fd94b56a612bdde1b196c11233b48d
Joshua Furman <jafbird52@gmail.com>
2023-05-30 15:35:08 -0700

    Spike traps are randomized now

--- Assets/Scripts/RoseBush.cs
+++ Assets/Scripts/RoseBush.cs
@@ -7,8 +7,19 @@ public class RoseBush : MonoBehaviour
 
 	    public GameManager gm;
     	public LivesManager lm;
+        public Animator am;
 
-    	
+    public void Start()
+    {
+        //get the animator component
+        am = GetComponent<Animator>();
+
+        if (am != null)
+        {
+            //start the coroutine
+            StartCoroutine(spawnEm());
+        }
+    }
        public void OnTriggerEnter2D(Collider2D collision)
     {
     	if (collision.gameObject.tag == "Player")
@@ -18,5 +29,12 @@ public class RoseBush : MonoBehaviour
     		lm.TakeLife();
         
         }
+    }
+
+    public IEnumerator spawnEm()
+    {
+        am.enabled = false;
+        yield return new WaitForSeconds(Random.Range(1f, 3f));
+        am.enabled = true;
     }
 }
