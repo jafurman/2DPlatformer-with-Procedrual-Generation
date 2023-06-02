c1baa2d31f9867b4e6a614d8f13476ef6710e412
Joshua Furman <jafbird52@gmail.com>
2023-06-01 19:36:05 -0700

    Added more to boss level. Need to work on phase 2.

--- Assets/SkeletonBoss.cs
+++ Assets/SkeletonBoss.cs
@@ -19,6 +19,8 @@ public class SkeletonBoss : MonoBehaviour
 
     public Animator bossAnimator;
 
+    public GameObject healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
