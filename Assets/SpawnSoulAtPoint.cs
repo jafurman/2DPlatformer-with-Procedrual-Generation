5adab6ec4e1778e000c56a6b45dc2c0b588d382d
Joshua Furman <jafbird52@gmail.com>
2023-08-10 23:02:46 -0700

    Added soul spawner and spiders to level 4 maze

--- Assets/SpawnSoulAtPoint.cs
+++ Assets/SpawnSoulAtPoint.cs
@@ -6,13 +6,27 @@ public class SpawnSoulAtPoint : MonoBehaviour
 {
     public GameObject soul;
     public Transform spawnPoint;
+    public Transform spawnPoint2;
+
+    public Animator anim;
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-       if ( other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
-            Instantiate(soul, spawnPoint.position, spawnPoint.rotation);
+            anim.SetTrigger("copy");
         }
+    }
+
+    public void SpawnSouls()
+    {
+        Instantiate(soul, spawnPoint.position, spawnPoint.rotation);
+        Instantiate(soul, spawnPoint2.position, spawnPoint.rotation);
     }
 
 }
