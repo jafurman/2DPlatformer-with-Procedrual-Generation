34f10385f10e8938bbb79e9f9bc820c93f7fb1ca
Joshua Furman <jafbird52@gmail.com>
2023-08-21 13:05:27 -0700

    Added more to dialogue as well as a better way to transition between levels

--- Assets/SetActiveDialogue.cs
+++ Assets/SetActiveDialogue.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SetActiveDialogue : MonoBehaviour
 {
     public GameObject totalCanvas;
+    public Animator dialogueButton;
 
     private void Start()
     {
@@ -16,6 +17,7 @@ public class SetActiveDialogue : MonoBehaviour
         if (col.tag == "Player")
         {
             totalCanvas.SetActive(true);
+            dialogueButton.SetBool("SlideIn", true);
         }
 
     }
@@ -25,6 +27,7 @@ public class SetActiveDialogue : MonoBehaviour
         if (col.tag == "Player")
         {
             totalCanvas.SetActive(false);
+            dialogueButton.SetBool("SlideIn", false);
         }
 
     }
