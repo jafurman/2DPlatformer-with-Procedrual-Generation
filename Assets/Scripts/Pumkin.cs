02c99ab716bf93aa2a1cdd85531d556a60533da8
Joshua Furman <jafbird52@gmail.com>
2023-08-21 01:52:55 -0700

    Did alot for scene transition

--- Assets/Scripts/Pumkin.cs
+++ Assets/Scripts/Pumkin.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pumkin : MonoBehaviour
 {
 	public GameManager gm;
     public GameObject victoryScreen;
+    public GameObject player;
+    public string nextLevel;
 
+
+    public void Start()
+    {
+        victoryScreen.SetActive(false);
+    }
 
     public void OnTriggerStay2D(Collider2D other)
     {
     	if (other.gameObject.tag == "Player")
     	{
-    		Debug.Log("EPIC W");
+
             victoryScreen.SetActive(true);
+            if (PlayerController.FacingRight && Input.GetKeyDown(KeyCode.W))
+            {
+                SceneManager.LoadScene(nextLevel);
+            }
         }
 
             
--- Assets/Scripts/SinusoidalMove.cs
+++ Assets/Scripts/SinusoidalMove.cs
@@ -26,6 +26,7 @@ public class SinusoidalMove : MonoBehaviour
 
 
 
+
     // Start is called before the first frame update
     void Start()
     {
