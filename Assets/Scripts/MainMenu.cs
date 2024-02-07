0cfa8420a7e050eaa20a746ec06eadc3a7461cb6
Joshua Furman <jafbird52@gmail.com>
2024-02-06 20:06:38 -0800

    Start of upgrades system with upgrade-able slots

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,11 +11,15 @@ public class MainMenu : MonoBehaviour
 
 	public int defaultLives;
 	public int defaultScore;
-
+	public int defaultSoulSlots = 5;
+	public int defaultSoulScore = 0;
 	private void Start() 
 	{
 			PlayerPrefs.SetInt("CurrentLives", defaultLives);
 			PlayerPrefs.SetInt("CurrentScore", defaultScore);
+			PlayerPrefs.SetInt("CurrentSoulSlots", defaultSoulSlots);
+			PlayerPrefs.SetInt("soulScore", defaultSoulScore);
+
 	}
 	public void playGame()
 	{
--- Assets/SoulScoreManager.cs
+++ Assets/SoulScoreManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class SoulScoreManager : MonoBehaviour
 {
+    public int soulSlots;
     public Text addedPoints;
     public Text change;
 
@@ -34,6 +35,44 @@ public class SoulScoreManager : MonoBehaviour
         PlayerPrefs.Save();
         Debug.Log("Change: " + PlayerPrefs.GetInt("soulScore"));
         latest = points;
-        
     }
+
+    public void subtractPoints(int points)
+    {
+
+        int currentScore = PlayerPrefs.GetInt("soulScore");
+        currentScore -= points;
+        PlayerPrefs.SetInt("soulScore", currentScore);
+        PlayerPrefs.Save();
+        Debug.Log("Change: " + PlayerPrefs.GetInt("soulScore"));
+        latest = points;
+    }
+
+    //tie these functions to buttons of this class instance in the HUD.. its a decent system I've set up.
+    public void buyHpSlot()
+    {
+
+        subtractPoints(1000);
+        Weapon.shotsLeft++;
+        int currentSlots = Weapon.shotsLeft;
+        PlayerPrefs.SetInt("CurrentSoulSlots", currentSlots);
+        PlayerPrefs.Save();
+        Debug.Log("New soul slot added. Now: " + currentSlots);
+    }
+
+    public void buyMobilitySlot()
+    {
+
+        subtractPoints(1000);
+        PlayerController.speed += .5f;
+        float currentSpeed = PlayerController.speed;
+        //scythe speed
+    }
+
+
+    //CONSIDER USING PLAYER PREFS FOR MAGIC, HP, and AGILITY slots ? 5 upgrades each? subtract player pref avaliable slots when clicking one?
+
+   
+
+
 }
