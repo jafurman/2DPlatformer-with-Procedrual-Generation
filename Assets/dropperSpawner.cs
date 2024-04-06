3bad4d7e6527ebc4ba3114a84f6683b4eff37739
Joshua Furman <jafbird52@gmail.com>
2024-04-05 21:27:17 -0700

    Added in more additions to the boss fight. There is now a dropper spawner and I can start making pattern drops with my prefab drop styles. It's cool.

--- Assets/dropperSpawner.cs
+++ Assets/dropperSpawner.cs
@@ -9,12 +9,21 @@ public class dropperSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        RandomPositionDrop();
+        StartCoroutine(RandomDropWave());
     }
 
     public void RandomPositionDrop()
     {
         // randomly choose 10 to drop from above
+        foreach(GameObject go in positions)
+        {
+            Vector2 pos = go.transform.position;
+            int chanceToSpawn = Random.Range(0, 10);
+            if (chanceToSpawn < 4 )
+            {
+                Instantiate(droppedItem, pos, Quaternion.identity);
+            }
+        }
     }
 
     public void DropEveryOther()
@@ -31,6 +40,145 @@ public class dropperSpawner : MonoBehaviour
 
         }
     }
+
+    public void DropEveryThird()
+    {
+        int counter = 0;
+        foreach (GameObject go in positions)
+        {
+            counter++;
+            if (counter % 3 == 0)
+            {
+                Vector2 pos = go.transform.position;
+                Instantiate(droppedItem, pos, Quaternion.identity);
+            }
+
+        }
+    }
+
+    public void DropRightHalf()
+    {
+        int counter = 0;
+        foreach (GameObject go in positions)
+        {
+            counter++;
+            if (counter >= 11)
+            {
+                Vector2 pos = go.transform.position;
+                Instantiate(droppedItem, pos, Quaternion.identity);
+            }
+
+        }
+    }
+
+    public void DropLeftHalf()
+    {
+        int counter = 0;
+        foreach (GameObject go in positions)
+        {
+            counter++;
+            if (counter <= 11)
+            {
+                Vector2 pos = go.transform.position;
+                Instantiate(droppedItem, pos, Quaternion.identity);
+            }
+
+        }
+    }
+
+    //Picks A Random Drop for 5 Drops Worth
+    public IEnumerator RandomDropWave()
+    {
+        int counter = 0;
+        foreach (GameObject go in positions)
+        {
+            counter += 1;
+            if (counter >= 5)
+            {
+                break;
+            }
+            int random = Random.Range(1, 7);
+            if (random == 1)
+            {
+                DropEveryThird();
+                yield return new WaitForSeconds(5f);
+            }
+            else if (random == 2)
+            {
+                DropEveryOther();
+                yield return new WaitForSeconds(5f);
+            }
+            else if (random == 3)
+            {
+                RandomPositionDrop();
+                yield return new WaitForSeconds(5f);
+            }
+            else if (random  == 4)
+            {
+                StartCoroutine(LeftWipeDrop());
+                yield return new WaitForSeconds(6.2f);
+            }
+            else if (random == 5)
+            {
+                StartCoroutine(RightWipeDrop());
+                yield return new WaitForSeconds(6.2f);
+            }
+            else if (random == 6)
+            {
+                DropLeftHalf();
+                yield return new WaitForSeconds(5f);
+            }
+            else if (random == 7)
+            {
+                DropRightHalf();
+                yield return new WaitForSeconds(5f);
+            }
+
+        }
+
+    }
+
+    public IEnumerator LeftWipeDrop()
+    {
+        int counter = 0;
+        foreach (GameObject go in positions)
+        {
+            counter += 1;
+            if (counter == 12)
+            {
+                continue;
+            }
+            else
+            {
+                Vector2 pos = go.transform.position;
+                yield return new WaitForSeconds(.2f);
+                Instantiate(droppedItem, pos, Quaternion.identity);
+            }
+
+        }       
+    }
+
+    public IEnumerator RightWipeDrop()
+    {
+        for (int i = positions.Length - 1; i >= 0; i--)
+        {
+            GameObject go = positions[i];
+            int counter = positions.Length - i;
+
+            if (counter == 10)
+            {
+                continue;
+            }
+            else
+            {
+                Vector2 pos = go.transform.position;
+                yield return new WaitForSeconds(.2f);
+                Instantiate(droppedItem, pos, Quaternion.identity);
+            }
+        }
+    }
+
+
 
 
 
